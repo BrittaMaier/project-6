51157e272394634f90f7cb2ed3bf7701f238ee02
Britta <britta.3.maier@uni-konstanz.de>
2025-09-20 13:44:13 +0200

    Add: PUT and DELETE to front and backend

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -5,16 +5,36 @@ export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
 
-  if (request.method === "GET") {
-    const place = await Place.findById(id);
+  try {
+    if (request.method === "GET") {
+      const place = await Place.findById(id);
 
-    if (!place) {
-      response.status(404).json({ status: "Not found" });
+      if (!place) {
+        response.status(404).json({ status: "Not found" });
+        return;
+      }
+
+      response.status(200).json(place);
       return;
     }
-
-    response.status(200).json(place);
-    return;
+    if (request.method === "PUT") {
+      const placeData = request.body;
+      const place = await Place.findByIdAndUpdate(id, placeData);
+      if (!place) {
+        response.status(404).json({ status: "Not found" });
+        return;
+      }
+      response.status(200).json({ status: "Place updated" });
+      return;
+    }
+    if (request.method === "DELETE") {
+      await Place.findByIdAndDelete(id);
+      response.status(200).json({ message: "Place deleted succefully" });
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: "Internal Server Error" });
   }
   response.status(405).json({ status: "Method not allowed" });
 }
